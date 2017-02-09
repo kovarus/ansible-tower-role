4c5dde4e2a356f6b8f807713f1cdabee534dd47f
as0bu <towen27@gmail.com>
2017-02-09 11:07:23 -0700

    Adding comments to module code

--- library/tower_config.py
+++ library/tower_config.py
@@ -6,6 +6,17 @@ import json
 
 
 def tower_argument_spec():
+    """Adds additional arguments to the url_argument_spec for Tower
+
+    All module options should be defined here.
+
+    Args:
+        None
+
+    Returns:
+        This returns the argument_spec dictionary for the tower_config module
+    """
+
     argument_spec = url_argument_spec()
     argument_spec.update(dict(
         host=dict(required=True, type='str'),
@@ -18,6 +29,16 @@ def tower_argument_spec():
 
 
 def get_authtoken(module):
+    """Retrieves an authenication token from the Tower API
+
+    Args:
+        module (obj): This the Ansible module object after options have been
+            passed.
+
+    Returns:
+        Authentication token in a string format.
+    """
+
     host = module.params['host']
     user = module.params['url_username']
     password = module.params['url_password']
@@ -40,6 +61,18 @@ def get_authtoken(module):
 
 
 def upload_license(module, ul, token):
+    """Uploads the Tower license
+
+    Args:
+        module (obj): This the Ansible module object after options have been
+            passed.
+        ul (dict): This is the Tower user license in a dictionary format.
+        token (str): The authentiction token for the Tower API.
+
+    Returns:
+        The HTTP status code of the license upload API call.
+    """
+
     host = module.params['host']
     url = "https://{0}/api/v1/config/".format(host)
 
@@ -56,6 +89,17 @@ def upload_license(module, ul, token):
 
 
 def compare_license(ul, tl):
+    """Compares two Tower licenses
+
+    Args:
+        ul (dict): This is a Tower user license in a dictionary format.
+        tl (dict): This is a Tower user license in a dictionary format.
+
+    Returns:
+        Boolean: True: if the licenses are different.
+                 False: if the license are the same.
+    """
+
     compare_list = ['company_name', 'contact_email', 'contact_name',
                     'hostname', 'instance_count', 'license_date',
                     'license_key', 'license_type', 'subscription_name',
@@ -70,6 +114,14 @@ def compare_license(ul, tl):
 
 
 def get_license_file(path):
+    """Retrieves the license from a file path
+
+    Args:
+        path (str): The file system path to the license file in json format.
+
+    Returns:
+        Dictionary of the json file
+    """
 
     with open(path) as license_data:
         d = json.load(license_data)
@@ -77,6 +129,17 @@ def get_license_file(path):
 
 
 def get_license(module, token):
+    """Retrieves the license from the active Tower configuration
+
+    Args:
+        module (obj): This the Ansible module object after options have been
+            passed.
+        token (str): The authentiction token for the Tower API.
+
+    Returns:
+        Dictionary of the license file from Tower.
+    """
+
     host = module.params['host']
     url = "https://{0}/api/v1/config/".format(host)
 
@@ -90,6 +153,18 @@ def get_license(module, token):
 
 
 def tower_license(module, token):
+    """The "main" of handling the Tower license logic
+
+    Args:
+        module (obj): This the Ansible module object after options have been
+            passed.
+        token (str): The authentiction token for the Tower API.
+
+    Returns:
+        A summary output of the function as a string
+        A resulting status of the funtion as a string
+    """
+
     license_path = module.params['license']
     user_license = get_license_file(license_path)
 
@@ -111,6 +186,17 @@ def tower_license(module, token):
 
 
 def main():
+    """The main function of handling the Tower configuration logic.
+
+    This function is used to take the output from all configurations done with
+        this module and parse for errors or changes. The result is passed back
+        to Ansible as a dictionary.
+
+    Args:
+        None
+    Returns:
+        None
+    """
 
     argument_spec = tower_argument_spec()
     module = AnsibleModule(argument_spec=argument_spec)
